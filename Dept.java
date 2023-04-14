4ef2fbac61a3519d521fc761be24c59ca5c140bf
vanajabobby86 <ivanamax23@gmail.com>
2023-04-14 15:14:53 +0530

    college

---
+++ Dept.java
@@ -0,0 +1,16 @@
+
+package org.college;
+
+public class Dept {
+	public void deptName() {
+		System.out.println("enter depatment name");
+	}
+	public static void main(String[] args) {
+		Dept deptdetails=new Dept();
+		deptdetails.deptName();
+	}
+		// TODO Auto-generated method stub
+
+	}
+
+
